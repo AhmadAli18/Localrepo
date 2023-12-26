0b8f67c3447ab4cb1ee2143a424218521f0750c6
AhmadAli18 <aliahmad33k@gmail.com>
2023-12-26 19:00:54 +0530

    add new feature

--- cal.cpp
+++ cal.cpp
@@ -5,6 +5,8 @@ int main(){
     int a,b;
     cin>>a>>b;
     int c = a +b;
+    int d = a*b;
     cout<<"The sum of the two numbers is :"<<c;
+    cout<<"The Product of the two numbers is:"<<d;
     return 0;
 }
